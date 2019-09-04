bdd4342348abd41964a62e38a27f41848de11d8a
amoux <carlosdeveloper2@gmail.com>
2019-09-03 21:59:42 -0400

    updated methods for the ngram module

--- david/models/lda.py
+++ david/models/lda.py
@@ -1,58 +1,78 @@
 
 import pandas as pd
 
+TOPIC_COLUMN_NAMES = ['dominant_topic', 'contribution(%)', 'keywords']
 
-def format_sent_topics(LDA_model, gensim_dict, corpus, num_topics: int):
-    """(LDA) Gets the dominant topic, percentage contribution and
+
+def get_lda_topics(LDA_model, Gensim_dict,
+                   text_col, num_topics, topic_col_names: list
+                   ):
+    '''(LDA) Gets the dominant topic, percentage contribution and
     the keywords for each given text document. This function identifies
     a topic related to the given text documents and flags all text items
     with high probability for a keyword topic.
 
-    PARAMETERS
+    NOTE: Pass a list containing three names for the topic column names to
+    override the defaults in the following order:
+
+    `TOPIC_COLUMN_NAMES=['dominant_topic', 'contribution(%)', 'keywords']`
+
+    Parameters:
     ----------
-    LDA_model : (object)
+
+    `LDA_model` : (object)
         A trained LDA (Latent Dirichlet Allocation) model.
 
-    gensim_dict : (object)
+    `Gensim_dict` : (object)
         A gensim dictionary object from the corpora.Dictionary
         module. NOTE: The dict must be passed to gensim before
         passing it to this function.
 
-    corpus : (df)
+    `text_col` : (list[str])
         The text column in a pandas.Dataframe containing texts.
         The text must be preprocessed and vectorized before using
         this function.
 
-    num_topics : (int)
+    `num_topics` : (int)
         Number of topics keywords to assing to each
-        text sentence in the corpus
+        text sentence in the text_col
 
-    Returns
+    Returns:
     -------
         Returns a pandas.Dataframe containing tokenized topic text sentences
         and the document's dominant (top) keywords.
-    """
-    # dataframe instance for the topics
-    df_topics = pd.DataFrame()
-    # get main topic in each document
-    for i, doc in enumerate(LDA_model[gensim_dict]):
+
+    '''
+    if topic_col_names and len(topic_col_names) == 3:
+        TOPIC_COLUMN_NAMES = topic_col_names
+
+    # dataframe instance for the topics.
+    df = pd.DataFrame()
+
+    # get main topic in each document.
+    for _, doc in enumerate(LDA_model[Gensim_dict]):
         doc_row = doc[0] if LDA_model.per_word_topics else doc
         doc_row = sorted(doc_row, key=lambda x: (x[1]), reverse=True)
-        # get the top dominant topics
-        for topic, (topic_idx, topic_probability) in enumerate(doc_row):
-            if topic == 0:
-                # get a representation for selected topics
-                topic_terms = LDA_model.show_topic(topic_idx)
+
+        # get the top dominant topics.
+        for idx, (topic, topic_probability) in enumerate(doc_row):
+            if idx == 0:
+                # get a representation for selected topics.
+                topic_terms = LDA_model.show_topic(topic)
                 topic_keywords = ", ".join([w for w, prop in topic_terms])
-                df_topics = df_topics.append(pd.Series([
-                    int(topic_idx),
-                    round(topic_probability, num_topics), topic_keywords]
+                df = df.append(pd.Series(
+                    [
+                        int(topic),
+                        round(topic_probability, num_topics),
+                        topic_keywords
+                    ]
                 ), ignore_index=True)
             else:
                 break
-    # column names for the topic dataframe instance
-    df_topics.columns = ['dominant_topic', 'contribution(%)', 'keywords']
-    # add original text to the end of the output
-    df_contents = pd.Series(corpus)
-    df_topics = pd.concat([df_topics, df_contents], axis=1)
-    return(df_topics)
+
+    # assign the column names for the topic dataframe instance.
+    df.columns = TOPIC_COLUMN_NAMES
+    # add original text to the end of the output.
+    df_contents = pd.Series(text_col)
+    df = pd.concat([df, df_contents], axis=1)
+    return (df)
--- david/models/ngrams.py
+++ david/models/ngrams.py
@@ -1,4 +1,107 @@
+from typing import Iterable, List
+
+import gensim
+import spacy
+from gensim.utils import simple_preprocess
 from sklearn.feature_extraction.text import CountVectorizer
+from spacy import lang
+
+SPACY_MODEL = 'en_core_web_lg'
+SPACY_DISABLE = ['parser', 'ner']
+SPACY_POSTAGS = ['NOUN', 'ADJ', 'VERB', 'ADV']
+STOP_WORDS = lang.en.stop_words.STOP_WORDS
+
+
+def _prep(stopwords: set, sentences: list):
+    # removes stop-words and preprocess texts with gensim.
+    texts = [[word for word in simple_preprocess(str(sent))
+              if word not in stopwords] for sent in sentences]
+    return texts
+
+
+def text2ngrams(sentences: Iterable[str],
+                spacy_model: str = SPACY_MODEL,
+                disable: list = SPACY_DISABLE,
+                stop_words: set = STOP_WORDS,
+                post_tags: list = SPACY_POSTAGS,
+                min_count: int = 5,
+                threshold: float = 10.0
+                ) -> List[str]:
+    '''
+    Convert texts to N-grams (Spacy & Gensim).
+
+    Removes stopwords, forms (bigrams & trigrams), and lemmatizes texts.
+    This method uses the set of STOP_WORDS from spaCy's lang module and the
+    gensim.utils.simple_preprocess method, which converts a document
+    into a list of lowercase tokens, ignoring tokens that are too short
+    or too long.
+
+    NOTE: The following default parameters can be overwritten:
+
+        * SPACY_MODEL = 'en_core_web_lg'
+        * SPACY_DISABLE = ['parser', 'ner']
+        * STOP_WORDS = spacy.lang.en.stop_words.STOP_WORDS
+        * SPACY_POSTAGS = ['NOUN', 'ADJ', 'VERB', 'ADV']
+
+    Parameters:
+    ----------
+
+    `sentences` : (iterable of str)
+        A processed text data with: data_ready = text2ngrams(sentences).
+
+    `spacy_model` : (spacy language model, default='en_core_web_lg')
+        Other spacy models are compatible with this function.
+
+    `min_count` (Type[int|float], default=5)
+        Ignore all words and bigrams with total collected count lower than
+        this value. Method from class `gensim.models.phrases.Phrases`
+
+    `threshold` (float, default=10.0)
+         Represent a score threshold for forming the phrases (higher means
+         fewer phrases). A phrase of words a followed by b is accepted if
+         the score of the phrase is greater than threshold. Heavily depends
+         on concrete scoring-function, see the scoring parameter. Method from
+         class `gensim.models.phrases.Phrases`
+
+    Returns:
+    -------
+        Returns preprocessed texts.
+
+    '''
+    if spacy_model:
+        SPACY_MODEL = spacy_model
+    if disable:
+        SPACY_DISABLE = disable
+    if stop_words:
+        STOP_WORDS = stop_words
+    if post_tags:
+        SPACY_POSTAGS = post_tags
+
+    # build biagrams and trigram models
+    bigram = gensim.models.Phrases(sentences, min_count, threshold)
+    trigram = gensim.models.Phrases(bigram[sentences], threshold=threshold)
+    bigram_mod = gensim.models.phrases.Phraser(bigram)
+    trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+    texts = _prep(STOP_WORDS, sentences)
+    texts = [bigram_mod[text] for text in texts]
+    texts = [trigram_mod[bigram_mod[text]] for text in texts]
+
+    nlp = spacy.load(SPACY_MODEL, disable=SPACY_DISABLE)
+    spacy.prefer_gpu()
+
+    texts_out = []
+    for sent in texts:
+        doc = nlp(' '.join(sent))
+        texts_out.append(
+            [
+                token.lemma_ for token in doc
+                if token.pos_ in SPACY_POSTAGS
+            ]
+        )
+    # remove stopwords once more after lemmatization
+    texts_out = _prep(STOP_WORDS, texts_out)
+    return texts_out
 
 
 def n_grams(corpus: list,
@@ -7,7 +110,9 @@ def n_grams(corpus: list,
             max_features: int = None,
             reverse: bool = True):
     '''
-    N-Gram CountVectorizer for the most frequently occurring words.
+    N-gram Frequency with Sklearn CountVectorizer.
+
+    Returns the most frequently occurring words.
 
         * unigrams-range: (1, 1)
         * biagrams-range: (2, 2)
@@ -30,6 +135,9 @@ def n_grams(corpus: list,
 def top_unigrams(corpus, n=5, max_features=None, reverse=True):
     '''N-Gram CountVectorizer most frequently used Unigrams.
 
+    Usage:
+    -----
+
         >>> top_words = top_unigrams(corpus, n=5)
         >>> top_df = pd.DataFrame(top_words)
         >>> top_df.columns = ["unigram", "frequency"]
@@ -40,6 +148,9 @@ def top_unigrams(corpus, n=5, max_features=None, reverse=True):
 def top_bigrams(corpus, n=5, ngram_range=(2, 2),
                 max_features=2000, reverse=True):
     '''N-Gram CountVectorizer most frequently used Bi-grams.
+
+    Usage:
+    -----
 
         >>> bigram_words = top_bigrams(corpus, n=5)
         >>> bigram_df = pd.DataFrame(top_bigram_words)
@@ -53,6 +164,9 @@ def top_trigrams(corpus, n=5, ngram_range=(3, 3),
                  max_features=2000, reverse=True):
     '''N-Gram CountVectorizer most frequently used Tri-grams.
 
+    Usage:
+    -----
+
         >>> triagram_words = top_trigrams(corpus, n=5)
         >>> triagram_df = pd.DataFrame(top_triagram_words)
         >>> triagram_df.columns = ["tri-gram", "frequency"]
@@ -64,6 +178,9 @@ def top_trigrams(corpus, n=5, ngram_range=(3, 3),
 def top_quadgrams(corpus, n=5, ngram_range=(4, 4),
                   max_features=2000, reverse=True):
     '''N-Gram CountVectorizer most frequently used Quad-grams.
+
+    Usage:
+    -----
 
         >>> quadgram_words = top_quadgrams(corpus, n=5)
         >>> quadgram_df = pd.DataFrame(top_quadgram_words)
