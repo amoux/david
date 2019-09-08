58a27d81f1209307d9804c32bb0af1ca45770405
neuralmachine <carlosdeveloper2@gmail.com>
2019-09-08 18:09:17 -0400

    Update labelers.py

--- david/lang/labelers.py
+++ david/lang/labelers.py
@@ -1,92 +1,93 @@
 
-from nltk import sent_tokenize
-from nltk import word_tokenize
+import logging
+import os
+
 import numpy as np
+import pandas as pd
+import requests
+import spacy
+import tensorflow as tf
+import tensorflow_hub as tfHub
+from IPython.display import HTML
+from sklearn import preprocessing
+from spacy import displacy
+from spacy.lang.en import English
+
+logging.getLogger('tensorflow').disabled = True
 
 
-class TextLabeler():
-    def __init__(self, text, dict_list):
-        '''Assigns a label format to a given key value pair.
-
-        text : (str)
-            The text that contains the values to replace.
-
-        dict_list : (list)
-         A list of dict key value pairs [{'key': 'value'}]
-
-         >>> dict_list = [{'city':'Paris'}]
-         >>> text = "A trip to Paris"
-         >>> label_keys = TextLabeler(text, dict_list)
-         > "A trip to [Paris](city) : [value](key)"
-        '''
-        self.text = text
-        self.iterate(dict_list)
-
-    def replace_key_value(self, label):
-        '''Replace any occurrence of a value with the key
-        '''
-        for key, value in label.items():
-            label = """[{1}]({0})""".format(key, value)
-            self.text = self.text.replace(value, label)
-            return self.text
-
-    def iterate(self, dict_list):
-        '''Iterate over each dict object in a given list of dicts, `dict_list`
-        '''
-        for label in dict_list:
-            self.text = self.replace_key_value(label)
-        return self.text
+nlp = spacy.load('en_core_web_lg')
 
 
-class ReplaceKeyValues():
+class GoogleDriveDownloader:
+    URL = 'https://docs.google.com/uc?export=download'
+    DIR = 'data'
 
-    def __init__(self, text, dict_list):
-        self.text = text
-        self.iterate(dict_list)
+    def __init__(self, file_id: str, file_name='temp.p', chunk_size=32_768):
+        self.file_id = file_id
+        self.file_name = file_name
+        self.destination = os.path.join(self.DIR, file_name)
+        self.chunk_size = chunk_size
 
-    def replace_key_value(self, label):
-        '''Replace any occurrence of a value with the key
-        '''
-        for key, value in label.items():
-            self.text = self.text.replace(value, key)
-            return self.text
+    def _confirm_token(self, response):
+        for (key, val) in response.cookies.items():
+            if key.startswith('download_warning'):
+                return val
+        return None
 
-    def iterate(self, dict_list):
-        '''Iterate over each dict object in a given list of dicts, `dict_list`
-        '''
-        for label in dict_list:
-            self.text = self.replace_key_value(label)
-        return self.text
+    def _save_content(self, response):
+        if not os.path.exists(self.DIR):
+            os.makedirs(self.DIR)
+        with open(self.destination, 'wb') as f:
+            for chunk in response.iter_content(self.chunk_size):
+                if chunk:
+                    f.write(chunk)
+
+    def _load_dataframe(self):
+        df = pd.read_pickle(self.destination)
+        return df
+
+    def download_dataframe(self, stream=True, load_df=False):
+        session = requests.Session()
+        response = session.get(
+            self.URL, params={'id': self.file_id}, stream=stream)
+        token = self._confirm_token(response)
+        if token:
+            params = {'id': self.file_id, 'confirm': token}
+            response = session.get(self.URL, params=params, stream=stream)
+        self._save_content(response)
+        if load_df:
+            return self._load_dataframe()
 
 
-def create_batch(self, sentence_list):
-    """Create a batch for a list of sentences
-    """
-    embeddings_batch = []
+# def create_batch(self, sentence_list):
+#     """Create a batch for a list of sentences
+#     """
+#     embeddings_batch = []
 
-    for sent in sentence_list:
-        embeddings = []
-        sent_tokens = sent_tokenize(sent)
-        word_tokens = [word_tokenize(w) for w in sent_tokens]
-        tokens = [value for sublist in word_tokens for value in sublist]
-        tokens = [token for token in tokens if token != '']
+#     for sent in sentence_list:
+#         embeddings = []
+#         sent_tokens = sent_tokenize(sent)
+#         word_tokens = [word_tokenize(w) for w in sent_tokens]
+#         tokens = [value for sublist in word_tokens for value in sublist]
+#         tokens = [token for token in tokens if token != '']
 
-        for token in tokens:
-            embeddings.append(self.embdict.tok2emb.get(token))
+#         for token in tokens:
+#             embeddings.append(self.embdict.tok2emb.get(token))
 
-        if len(tokens) < self.max_sequence_length:
-            pads = [np.zeros(self.embedding_dim) for _ in range(
-                self.max_sequence_length - len(tokens)
-            )]
-            embeddings = pads + embeddings
-        else:
-            embeddings = embeddings[-self.max_sequence_length:]
+#         if len(tokens) < self.max_sequence_length:
+#             pads = [np.zeros(self.embedding_dim) for _ in range(
+#                 self.max_sequence_length - len(tokens)
+#             )]
+#             embeddings = pads + embeddings
+#         else:
+#             embeddings = embeddings[-self.max_sequence_length:]
 
-        embeddings = np.asarray(embeddings)
-        embeddings_batch.append(embeddings)
+#         embeddings = np.asarray(embeddings)
+#         embeddings_batch.append(embeddings)
 
-    embeddings_batch = np.asarray(embeddings_batch)
-    return embeddings_batch
+#     embeddings_batch = np.asarray(embeddings_batch)
+#     return embeddings_batch
 
 
 class SchoolMember:
