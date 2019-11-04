6d085d24a13af2b63e77a3fc9b5582837cc24b89
amoux <carlosdeveloper2@gmail.com>
2019-11-04 02:11:43 -0500

    update io.py
    all text writers must check sequence size by stripping any whitespace first.

--- david/utils/io.py
+++ david/utils/io.py
@@ -1,6 +1,6 @@
-import os
 import io as _io
 import json
+import os
 
 
 def as_txt_file(texts: list, fname: str, output_dir='.'):
@@ -9,7 +9,7 @@ def as_txt_file(texts: list, fname: str, output_dir='.'):
     file_path = os.path.join(output_dir, fname)
     with open(file_path, 'w', encoding='utf-8') as txt_file:
         for text in texts:
-            if len(text) > 0:
+            if len(text.strip()) > 0:
                 txt_file.write('%s\n' % text)
         txt_file.close()
 
@@ -21,6 +21,7 @@ def as_jsonl_file(texts: list, fname: str, output_dir='.'):
     with _io.open(file_path, 'w', encoding='utf-8') as jsonl:
         for text in texts:
             if len(text.strip()) > 0:
-                print(json.dumps({'text': text},
-                                 ensure_ascii=False), file=jsonl)
+                print(
+                    json.dumps({'text': text}, ensure_ascii=False), file=jsonl
+                )
         jsonl.close()
