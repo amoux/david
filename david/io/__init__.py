34d1a899c12dcf47688a0bceef7476aa0de24e67
amoux <carlosdeveloper2@gmail.com>
2019-12-02 02:15:41 -0500

    Typo

--- david/io/__init__.py
+++ david/io/__init__.py
@@ -1,3 +1,2 @@
 from .text import as_jsonl_file, as_txt_file
-from .utils import (File, GoogleDriveDownloader, delete_files, download_df,
-                    download_url_file)
+from .utils import File, GoogleDriveDownloader, delete_files, download_url_file
