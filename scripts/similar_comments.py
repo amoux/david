d955288ce9ae8f0273063e2a636c12781c9553dc
amoux <carlosdeveloper2@gmail.com>
2020-01-01 20:24:06 -0500

    Updated names

--- scripts/similar_comments.py
+++ scripts/similar_comments.py
@@ -1,11 +1,12 @@
 import spacy
 from david import CommentsSql, SimilarDocuments
 
-MIN_TEXT = 30
-MAX_TEXT = 200
+MIN_TEXT_LEN = 30
+MAX_TEXT_LEN = 200
 NGRAM_RANGE = (1, 3)
 TOP_K = 10
-QUERY = "Make a video on the new iphone"
+DB_QUERY = "%make a video%"
+SD_QUERY = "Make a video on the new iphone"
 
 
 def prep_text_batch(batch):
@@ -13,7 +14,7 @@ def prep_text_batch(batch):
     texts = []
     for q in batch:
         q = q.text.strip()
-        if len(q) > MIN_TEXT and len(q) < MAX_TEXT and q not in texts:
+        if len(q) > MIN_TEXT_LEN and len(q) < MAX_TEXT_LEN and q not in texts:
             texts.append(q)
     return texts
 
@@ -31,18 +32,19 @@ def to_sentences(texts):
 
 def main():
     db = CommentsSql('unbox')
-    batch = db.fetch_comments('%make a video%', 'id,text', 'id')
+    batch = db.fetch_comments(DB_QUERY, 'id,text', 'id')
+    # preprocess and filter all the comments fetched from the database.
     texts = prep_text_batch(batch)
     sents = to_sentences(texts)
+    # construct a SimilarDocuments object with the sentences and ngram range.
     sd = SimilarDocuments(sents, ngram=NGRAM_RANGE)
     sd.learn_vocab()
-    # sd.add_query(QUERY)
-    # print out the top n most similar documents:
-    for doc in sd.iter_similar(TOP_K, QUERY, True):
+    # print out the top n most similar documents to the query.
+    for doc in sd.iter_similar(TOP_K, SD_QUERY, True):
         if doc['sim']:
             print("sim: {} text: {}".format(doc['sim'], doc['text']))
-    # there should only be one query.
-    print(f"\n{sd.queries}\n")
+
+    print(f"\nsimilar to query: {sd.queries}\n")
 
 
 if __name__ == '__main__':
