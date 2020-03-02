55e10be2d5ab2ccc6902bc8c392a5875f06145bd
amoux <carlosdeveloper2@gmail.com>
2020-03-01 20:29:57 -0500

    Small usage enhancements.  dict can be loaded from an object, file path, or from an iterable

--- david/lang/__init__.py
+++ david/lang/__init__.py
@@ -1,4 +1,4 @@
 from .contractions import replace_contractions
-from .spelling import SpellCorrect
+from .spelling import Speller
 from .stopwords import (DAVID_STOP_WORDS, GENSIM_STOP_WORDS, NLTK_STOP_WORDS,
                         SPACY_STOP_WORDS)
--- david/lang/spelling.py
+++ david/lang/spelling.py
@@ -4,84 +4,102 @@ Copyright (c) 2007-2016 Peter Norvig MIT license:
 www.opensource.org/licenses/mit-license.php
 """
 
-import collections
 import re
+from collections import Counter
+from typing import Dict, List, Pattern
 
 
-class SpellCorrect(object):
-    """Spell-Correct by Peter Norvig."""
+class Speller:
+    """Spell correction based on Peter Norvig's implementation."""
 
-    DEFAULT_CORPUS = 'big.txt'
-    WORD_COUNTS = None
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    def __init__(self, corpus_path: str = None):
-        """SpellCorrect uses word frquencies for matching the _correct grammar
-        of a word.
+    def __init__(
+        self,
+        filepath: str = None,
+        document: List[str] = None,
+        word_count: Dict[str, int] = None,
+    ):
+        """Speller uses word counts as a metric for correcting words.
 
-        corpus_path : (str)
-            If the left as None, it imports big.txt file as
-            the dataset for _correct spelled words.
+        `filepath` : A file containing lines of string sequences.
+        `document` : An iterable document of string sequences.
+        `word_count` : An instance of `collections.Counter` with
+            existing word count pairs.
         """
-        self.corpus_path = corpus_path
-        if not self.corpus_path:
-            self.corpus_path = self.DEFAULT_CORPUS
-        self.corpus = self.tokens(open(self.corpus_path).read())
-        self.WORD_COUNTS = collections.Counter(self.corpus)
-        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        self.word_count = word_count
+        if filepath is not None:
+            self.word_count_from_file(filepath)
+        elif document is not None:
+            self.word_count_from_doc(document)
 
-    def most_common(self, top_n=10):
-        return self.WORD_COUNTS.most_common(top_n)
+    def word_count_from_file(self, filepath: str):
+        """Load and tokenize texts into word count pairs from a file."""
+        tokens = self.tokenize(open(filepath).read())
+        self.word_count = Counter(tokens)
 
-    def tokens(self, text):
-        """Get all the words from the corpus."""
-        return re.findall('[a-z]+', text.lower())
+    def word_count_from_doc(self, document: List[str]):
+        """Set the word count dictionary from a document of string sequences."""
+        tokens = []
+        for doc in document:
+            tokens.extend(self.tokenize(doc))
+        self.word_count = Counter(tokens)
 
-    def correct_text(self, text):
-        """Correct all the words within a text, returning the corrected
-        text."""
-        return re.sub('[a-zA-Z]+', self.correct_match, text)
+    def most_common(self, k=10):
+        """Return the most common words from the dictionary counter."""
+        return self.word_count.most_common(k)
 
-    def correct_match(self, match):
-        """Spell correct word in match, and preserve proper upper/lower/title
-        case."""
+    def tokenize(self, sequence: str):
+        """Regex based word tokenizer."""
+        return re.findall("[a-z]+", sequence.lower())
+
+    def correct_string(self, sequence: str):
+        """Return the correct spell form a string sequence."""
+        return re.sub("[a-zA-Z]+", self.correct_match, sequence)
+
+    def correct_match(self, match: Pattern[str]):
+        """Spell correct word in match, and preserve proper case."""
         word = match.group()
 
         def case_of(text):
-            """Return the case-function appropriate for text:
+            """Return the case-function appropriate for text."""
+            return (
+                str.upper
+                if text.isupper()
+                else str.lower
+                if text.islower()
+                else str.title
+                if text.istitle()
+                else str
+            )
 
-            upper, lower, title, or just str.
-            """
-            return (str.upper if text.isupper() else
-                    str.lower if text.islower() else
-                    str.title if text.istitle() else
-                    str)
         return case_of(word)(self._correct(word.lower()))
 
     def _known(self, words):
-        return {w for w in words if w in self.WORD_COUNTS}
+        return {w for w in words if w in self.word_count}
 
     def _edits0(self, word):
         return {word}
 
     def _edits1(self, word):
-
         def splits(word):
-            return [(word[:i], word[i:]) for i in range(len(word)+1)]
+            return [(word[:i], word[i:]) for i in range(len(word) + 1)]
 
         pairs = splits(word)
-        deletes = [a+b[1:] for (a, b) in pairs if b]
-        transposes = [a+b[1]+b[0]+b[2:] for (a, b) in pairs if len(b) > 1]
-        replaces = [a+c+b[1:] for (a, b) in pairs for c in self.alphabet if b]
-        inserts = [a+c+b for (a, b) in pairs for c in self.alphabet]
-
+        deletes = [a + b[1:] for (a, b) in pairs if b]
+        transposes = [a + b[1] + b[0] + b[2:] for (a, b) in pairs if len(b) > 1]
+        replaces = [a + c + b[1:] for (a, b) in pairs for c in self.alphabet if b]
+        inserts = [a + c + b for (a, b) in pairs for c in self.alphabet]
         return set(deletes + transposes + replaces + inserts)
 
     def _edits2(self, word):
         return {e2 for e1 in self._edits1(word) for e2 in self._edits1(e1)}
 
     def _correct(self, word):
-        candidates = (self._known(self._edits0(word)) or
-                      self._known(self._edits1(word)) or
-                      self._known(self._edits2(word)) or
-                      [word])
-        return max(candidates, key=self.WORD_COUNTS.get)
+        candidates = (
+            self._known(self._edits0(word))
+            or self._known(self._edits1(word))
+            or self._known(self._edits2(word))
+            or [word]
+        )
+        return max(candidates, key=self.word_count.get)
